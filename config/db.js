d0ae644601e21d27afb15eca9752c15931be6621
MrRushikesh <rusikeshingale7447@gmail.com>
2024-02-11 18:39:24 +0530

    add database name in config/db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const dbUrl = `mongodb+srv://test:test@cluster0.kibua9i.mongodb.net/`;
+const dbUrl = `mongodb+srv://test:test@cluster0.kibua9i.mongodb.net/zomato`;
 
 mongoose.connect(dbUrl)
 .then((res) => console.log("Database Connected Successfully..."))
